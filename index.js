d80fc3b5e081ca1cf297cd293de6d7f11ea85736
fourthperson <iankariithi@gmail.com>
2024-12-26 18:49:04 +0300

    loaded any jokes

--- index.js
+++ index.js
@@ -5,5 +5,11 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import axios from 'axios';
+
+export const appAxios = axios.create({
+    baseURL: 'https://v2.jokeapi.dev/joke/',
+    timeout: 150000,
+});
 
 AppRegistry.registerComponent(appName, () => App);
--- src/page/home/home.tsx
+++ src/page/home/home.tsx
@@ -1,15 +1,58 @@
-import React from 'react';
-import { StatusBar, StyleSheet, View } from 'react-native';
-import { primaryDarkColor, whiteColor } from '../../config/theme';
+import React, { useEffect, useState } from 'react';
+import { StatusBar, StyleSheet, Text, View } from 'react-native';
+import { mediumFont, primaryDarkColor, whiteColor } from '../../config/theme';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { Gesture, GestureDetector } from 'react-native-gesture-handler';
+import { appAxios } from '../../..';
 
 function HomePage() : React.JSX.Element {
+
+    const [isLoading, setIsLoading] = useState(false);
+    const [joke, setJoke] = useState({});
+
+    async function loadJoke() {
+        setIsLoading(true);
+        try {
+            const response = await appAxios.get('Any');
+            console.log(response);
+            if(response.status === 200) {
+              if(response.data.error === true) {return;}
+
+              var j = response.data;
+              if(j.type !== 'single') {
+                j.joke = j.setup + '\n\n' + j.delivery;
+              }
+              setJoke(j);
+            }
+        } catch(e) {
+            console.error(e);
+        }
+        setIsLoading(false);
+    }
+
+    const tapGesture = Gesture.Tap().onStart(() => {
+        console.log('Tapped!');
+        loadJoke();
+    });
+
+    useEffect(() => {
+        loadJoke();
+    }, []);
+
     return (
         <View style={styles.container}>
             <StatusBar backgroundColor={primaryDarkColor}/>
-            <SafeAreaView style={styles.centeredItems}>
-
-            </SafeAreaView>
+            <GestureDetector gesture={tapGesture}>
+                <SafeAreaView style={styles.centeredItems}>
+                        <View collapsable={false}>
+                            <Text style={styles.jokeText}>
+                                {
+                                    isLoading ? 'Loading...' : joke.joke.toString()
+                                }
+                            </Text>
+                        </View>
+                </SafeAreaView>
+            </GestureDetector>
         </View>
     );
 }
@@ -22,7 +65,12 @@ const styles = StyleSheet.create({
     centeredItems: {
         flex: 1,
         justifyContent:'center',
-            alignItems: 'center',
+        alignItems: 'center',
+        padding: 16,
+    },
+    jokeText: {
+        fontSize: 18,
+        fontFamily: mediumFont,
     },
 });
 
