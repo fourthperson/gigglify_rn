79db5468982f905c0bfa5c382061876da8597c66
fourthperson <iankariithi@gmail.com>
2025-02-20 14:00:54 +0300

    upgraded to rn-0.77.0

--- metro.config.js
+++ metro.config.js
@@ -7,21 +7,21 @@ const {assetExts, sourceExts} = defaultConfig.resolver;
  * Metro configuration
  * https://reactnative.dev/docs/metro
  *
- * @type {import('metro-config').MetroConfig}
+ * @type {import('@react-native/metro-config').MetroConfig}
  */
 
 const {
-    wrapWithReanimatedMetroConfig,
+  wrapWithReanimatedMetroConfig,
 } = require('react-native-reanimated/metro-config');
 
 const config = {
-    transformer: {
-        babelTransformerPath: require.resolve('react-native-svg-transformer'),
-    },
-    resolver: {
-        assetExts: assetExts.filter((ext) => ext !== 'svg'),
-        sourceExts: [...sourceExts, 'svg'],
-    },
+  transformer: {
+    babelTransformerPath: require.resolve('react-native-svg-transformer'),
+  },
+  resolver: {
+    assetExts: assetExts.filter(ext => ext !== 'svg'),
+    sourceExts: [...sourceExts, 'svg'],
+  },
 };
 
 //module.exports = mergeConfig(getDefaultConfig(__dirname), config);
